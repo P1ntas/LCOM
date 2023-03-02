c2cce757e6d615482d8d78d20b526331e189e08d
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-03-02 00:24:26 +0000

    First two functions done

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,39 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  uint8_t st;
+  uint16_t div = TIMER_FREQ / freq;
 
-  return 1;
+  if (timer < 19 | timer > TIMER_FREQ) return 1;
+
+  if (timer_get_conf(timer, &st) == 1) return 1;
+
+  uint8_t lsb, msb;
+
+  if (util_get_lsb(timer, &lsb) == 1) return 1;
+  if (util_get_msb(timer, &msb) == 1) return 1;
+
+  switch (timer) {
+  case 0:
+    st = ((st & 0x0F) | TIMER_LSB_MSB | TIMER_SEL0);
+    break;
+
+  case 1:
+    st = ((st & 0x0F) | TIMER_LSB_MSB | TIMER_SEL1);
+    break;
+
+  case 2:
+    st = ((st & 0x0F) | TIMER_LSB_MSB | TIMER_SEL2);
+    break;
+  
+  default:
+    return 1;
+  }
+
+  if (sys_outb(TIMER_CTRL, st) == 1 || sys_outb(TIMER_0 + timer, st) == 1) return 1;
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -59,24 +88,83 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
+
+  union timer_status_field_val val;
   
   switch (field) {
     case tsf_all:
-      /* code */
+      val.byte = st;
       break;
   
     case tsf_initial:
+      switch (st & 0x30) {
+      case TIMER_LSB:
+        val.in_mode = LSB_only;
+        break;
+
+      case TIMER_MSB:
+        val.in_mode = MSB_only;
+        break;
+
+      case TIMER_LSB_MSB:
+        val.in_mode = MSB_after_LSB;
+        break;
+      
+      default:
+      val.in_mode = INVAL_val;
+        return 1;
+      }
       break;
 
     case tsf_mode:
+      switch (st & 0x0E) {
+      case 0x00:
+        val.count_mode = 0;
+        break;
+
+      case 0x01:
+        val.count_mode = 1;
+        break;
+
+      case (0x02 | 0x06):
+        val.count_mode = 2;
+        break;
+
+      case (0x03 | 0x07):
+        val.count_mode = 3;
+        break;
+      
+      case 0x04:
+        val.count_mode = 4;
+        break;
+
+      case 0x05:
+        val.count_mode = 5;
+        break;
+      
+      default:
+        return 1;
+      }
       break;
 
     case tsf_base:
+      switch (st & 0x01) {
+        case TIMER_BCD:
+          val.bcd = true;
+          break;
+      
+        case TIMER_BIN:
+          val.bcd = false;
+          break;
+
+        default:
+          return 1;
+      }
       break;
 
     default:
       return 1;
   }
 
-  return 1;
+  return timer_print_config(timer, field, val);
 }
